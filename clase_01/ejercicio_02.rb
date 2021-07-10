0df0041c23c73699a8295d71ca0507fb5eb2f70a
Armando <armando.amr16@gmail.com>
2021-07-10 04:13:22 -0500

    Agregando ejercicio 2 y 3

---
+++ clase_01/ejercicio_02.rb
@@ -0,0 +1,9 @@
+#Ejercicio_02:
+#Escribir un programa que calcule el área de un triángulo
+
+base = 28.5
+altura = 10.5
+
+area = base * altura
+
+puts "El area es: #{area}"
---
+++ clase_01/ejercicio_03.rb
@@ -0,0 +1,8 @@
+#Ejercicio_03:
+#Escribir un programa que calcule el área de un cuadrado
+
+lado = 22.5
+
+area = lado ** 2
+
+puts "El area es: #{area}"

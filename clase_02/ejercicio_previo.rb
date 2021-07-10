09f623c4c5bc8c50e663c1858f3824d08f40067e
Armando <armando.amr16@gmail.com>
2021-07-10 04:41:33 -0500

    Agregando ejercicio previo

---
+++ clase_02/ejercicio_previo.rb
@@ -0,0 +1,7 @@
+#Ejercicio previo:
+#Escribir un programa que pida ingresar por teclado un monto en soles y lo devuelva convertido a dólares
+
+puts "Ingrese monto: "
+monto_soles = gets.to_f
+
+puts "Monto convertido en USD: #{monto_soles * 3.95}"

1747f284cf182344cbbc4ec723a2864bdd18e78f
Armando <armando.amr16@gmail.com>
2021-07-10 03:52:08 -0500

    Agregando Ejercicio 1 del rectangulo

---
+++ clase_01/ejercicio_01.rb
@@ -0,0 +1,10 @@
+#Ejercicio_01:
+#Escribir un programa que calcule el área de un rectángulo
+
+base = 10
+altura = 50
+
+area = base * altura
+
+#puts "El area es: " + area.to_s
+puts "El area es: #{area}"
